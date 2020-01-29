34c819dbf2352d03b843298d7ec205359a3a6196
Artyom Davydov <fax1ty.ru@gmail.com>
2020-01-29 22:05:22 +0400

    Discord функционал

--- config.ts
+++ config.ts
@@ -1,9 +1,11 @@
 export = {
     VK_GROUP_ID: 143954349,
     VK_API_KEY: process.env.VK_API_KEY,
+    VK_CHECK_RATE: 60,
 
     TELEGRAM_CHANNEL_ID: '@Samara_IT_Community',
     TELEGRAM_API_KEY: process.env.TELEGRAM_API_KEY,
+    DISCORD_ADMIN_ROLE: 'Валар',
 
     DISCORD_CHANNEL_NAME: 'general',
     DISCORD_API_KEY: process.env.DISCORD_API_KEY,
--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import Discord = require('discord.js');
 import VKParser = require('./libs/api/vk_api');
 import { VKPost } from './interfaces';
 import TelegramAPI = require('./libs/api/telegram_api');
-import config = require('./config');
+import config from './config';
 import { QueueManager, getTimestamp } from './libs/utils';
 let queueManager = new QueueManager();
 
@@ -23,8 +23,63 @@ export let discord = new Discord.Client();
 export let vk_parser = new VKParser({ token: config.VK_API_KEY, groupID: config.VK_GROUP_ID });
 export let telegram = new TelegramAPI(config.TELEGRAM_API_KEY);
 
+import fs from 'fs';
+
 discord.on('ready', () => {
     console.log(`Дискорд бот работает на аккаунте ${discord.user.tag}!`);
+});
+discord.on('message', msg => {
+    if (msg.channel.type == 'text' && msg.member.roles.has(config.DISCORD_ADMIN_ROLE) && msg.member.user != discord.user) {
+        let args = msg.content.split(' ');
+        switch (args[0]) {
+            case '$set':
+                if (args[1] == 'channel') {
+                    if (args[2]) {
+                        let newChannel = discord.guilds.first().channels.find(channel => channel.name == args[2]);
+                        if (newChannel) {
+                            config.DISCORD_CHANNEL_NAME = newChannel.name;
+                            fs.writeFileSync('./config.ts', fs.readFileSync('./config.ts', 'utf8').replace(new RegExp('DISCORD_CHANNEL_NAME: \'([^\s]+)\''), `DISCORD_CHANNEL_NAME: '${newChannel}'`))
+                            msg.channel.send(`Ура! Теперь я шлю сообщения на канале ${newChannel}`)
+                                .then(() => msg.delete(1000 * 2));
+                        }
+                        else msg.channel.send('Ошибка! Такого канала не существует')
+                            .then(() => msg.delete(1000 * 2));
+                    }
+                    else msg.channel.send('Ошибка! Вы не указали название нового канала')
+                        .then(() => msg.delete(1000 * 2));
+                }
+
+                if (args[1] == 'rate') {
+                    if (args[2]) {
+                        let time = parseInt(args[2]);
+                        if (time) {
+                            config.VK_CHECK_RATE = time;
+                            fs.writeFileSync('./config.ts', fs.readFileSync('./config.ts', 'utf8').replace(new RegExp('VK_CHECK_RATE: \'([^\s]+)\''), `VK_CHECK_RATE: '${time}'`))
+                            msg.channel.send(`Ура! Теперь я шлю сообщения раз в ${time} минут(у)`)
+                                .then(() => msg.delete(1000 * 2));
+                        }
+                        else msg.channel.send('Ошибка! Это не число')
+                            .then(() => msg.delete(1000 * 2));
+                    }
+                    else msg.channel.send('Ошибка! Вы не ввели число')
+                        .then(() => msg.delete(1000 * 2));
+                }
+
+                if (args[1] == 'name') {
+                    if (args[2]) {
+                        discord.user.setUsername(args.slice(2, args.length - 1).join(' '));
+                        msg.channel.send(`Ура! Теперь меня зовут ${args[2]}`)
+                            .then(() => msg.delete(1000 * 2));
+                    }
+                    else msg.channel.send('Ошибка! Вы не указали имя')
+                        .then(() => msg.delete(1000 * 2));
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
 });
 discord.login(config.DISCORD_API_KEY);
 
